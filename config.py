dffa932021ec10552b480cf94301d2cbf934c874
Athul <61957669+athul1706@users.noreply.github.com>
2021-09-24 14:53:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,8 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<code>{previouscaption}</code> \n ╾╾╾╾╾╾╾╾╾╾╼╼╼╼╼╼╼╼╼╼ \n \n<b>Group : <a href='https://t.me/InfameSeries'>𝐈𝐍𝐅𝐀𝐌𝐄 𝐒𝐄𝐑𝐈𝐄𝐒</a> </b>")
+
 
 #Set true if you want Disable your Channel Posts Share button
 if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
