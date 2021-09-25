4435ac8caaea904aee91abdcf474583e8cf9c15d
Athul <61957669+athul1706@users.noreply.github.com>
2021-09-25 15:50:55 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -159,45 +159,26 @@ async def subscribers_count(bot, m: Message):
 
 @Bot.on_message(filters.private & filters.command('send') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
-    if message.reply_to_message:
+    id = message.from_user.id
+    if id not in ADMINS:
+        return
+    if (" " not in message.text) and ("send" in message.text) and (message.reply_to_message is not None):
         query = await query_msg()
-        broadcast_msg = message.reply_to_message
-        total = 0
-        successful = 0
-        blocked = 0
-        deleted = 0
-        unsuccessful = 0
-        
-        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
         for row in query:
             chat_id = int(row[0])
             try:
-                await broadcast_msg.copy(chat_id)
-                successful += 1
+                await client.copy_message(
+                    chat_id=chat_id,
+                    from_chat_id=message.chat.id,
+                    message_id=message.reply_to_message.message_id,
+                    caption=message.caption,
+                    reply_markup=message.reply_markup
+                )
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                await broadcast_msg.copy(chat_id)
-                successful += 1
-            except UserIsBlocked:
-                blocked += 1
-            except InputUserDeactivated:
-                deleted += 1
-            except:
-                unsuccessful += 1
+            except Exception:
                 pass
-            total += 1
-        
-        status = f"""<b><u>Broadcast Completed</u>
-
-Total Users: <code>{total}</code>
-Successful: <code>{successful}</code>
-Blocked Users: <code>{blocked}</code>
-Deleted Accounts: <code>{deleted}</code>
-Unsuccessful: <code>{unsuccessful}</code></b>"""
-        
-        return await pls_wait.edit(status)
-
     else:
-        msg = await message.reply(REPLY_ERROR)
+        msg = await message.reply_text(REPLY_ERROR)
         await asyncio.sleep(8)
         await msg.delete()
